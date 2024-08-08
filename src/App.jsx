c5b77dadc06b32f3071f3c2748733a88d982947d
IM JAEYOUNG <118138003+ImJaePang@users.noreply.github.com>
2024-08-08 07:36:14 +0000

    learn react router dom and link

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import Home from "./routers/Home"
+import Detail from "./routers/Detail"
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return(
-    <Home/>
+    <Router>
+      <Routes>
+        <Route path="/" Component={Home} />
+        <Route path="/Detail" Component={Detail} />
+      </Routes>
+    </Router>
+
+    // <Home/>
+
+    
   )
 }
 
--- src/routers/Movies.jsx
+++ src/routers/Movies.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+
 function Movies ({id, title, rating, mediumCoverImage, summary, genres}){
 
     return (
     <div>
         <h2>{title}, rating: {rating}</h2>
-        <img src={mediumCoverImage} alt={title} />
+        <Link to="/Detail"><img src={mediumCoverImage} alt={title} /></Link>
         <p>{summary}</p>
         <ul>
             {genres.map(g => <li key={g}>{g}</li>)}
